0773b0009e0c0685ab616094b8ce8c2b4851b6fa
Sudeep Vundurty <sudeepmailin@gmail.com>
2025-04-28 01:09:49 +0530

    added sign in page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-row items-center justify-center h-screen w-full space-x-12 text-gray-800 font-inter">
+    <img src="/images/1.png" alt="Hero" width="600" />
+    <div class="flex flex-col items-center justify-center p-10">
+        <p class="font-logo text-[4rem] mb-10">Instapound</p>
+        <input type="email" placeholder="Email" class="border border-stone-300 p-2 w-68 placeholder:text-left font-[350] placeholder:font-[350 placeholder:text-stone-500 mb-2 bg-gray-50 text-xs rounded-xs outline-0"/>
+        <input type="password" placeholder="Password" class="border border-stone-300 p-2 w-68 placeholder:text-left font-[350] placeholder:font-[350] placeholder:text-stone-500 mb-2 bg-gray-50 text-xs rounded-xs outline-0" />
+        <button class="text-white text-sm p-2 w-68 mt-2 rounded-md bg-sky-500 hover:bg-blue-500 hover:cursor-pointer font-[600]">Log in</button>
+        <div class="flex items-center my-4">
+            <hr class="w-28 border-t border-gray-300" />
+            <span class="mx-4 text-gray-500 text-sm font-[500]">OR</span>
+            <hr class="w-28 border-t border-gray-300" />
+        </div>
+        <a href="/" class="font-[400] text-sm my-10">Forgot password?</a>
+        <div class="flex flex-row items-center text-sm gap-1">
+            <p>Don't have an account?</p>
+            <a href="/" class="text-sky-500 font-[500]">Sign up</a>
+        </div>
+    </div>    
+</div>
